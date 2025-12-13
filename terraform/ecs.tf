ab7735435b833b9bf754c18e54973090f186fe78
soundaryanarayana6 <soundaryan2063@gmail.com>
2025-12-13 23:11:09 +0530

    fix issues

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -27,6 +27,34 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+
+resource "aws_iam_role_policy" "ecs_ecr_access" {
+  name = "${var.environment}-ecs-ecr-access"
+  role = aws_iam_role.ecs_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "${aws_ecr_repository.app_repo.arn}"
+      }
+    ]
+  })
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = "${var.environment}-app"
   network_mode             = "awsvpc"
